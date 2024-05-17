04e08f6e92f4cd56b5e794b4de7ba4c6d2182ec0
junsu0302 <junsu96378224@gmail.com>
2024-05-17 15:55:18 +0900

    리펙토링 : 주석 해제

--- Financial-Analysis/Financial_time-series/Correlation_analysis.py
+++ Financial-Analysis/Financial_time-series/Correlation_analysis.py
@@ -14,11 +14,11 @@ data = raw_data[['.SPX', '.VIX']].dropna()
 # TODO: 로그 수익률
 rets = np.log(data / data.shift(1))
 rets.dropna(inplace=True)
-# pd.plotting.scatter_matrix(rets, #? 데이터
-#                            alpha=0.2, #? 투명도 설정 
-#                            diagonal='hist', #? 대각선 부분 그래프
-#                            hist_kwds={'bins': 35}, #? 그래프에 넣은 인수 키워드
-#                            figsize=(10,6))
+pd.plotting.scatter_matrix(rets, #? 데이터
+                           alpha=0.2, #? 투명도 설정 
+                           diagonal='hist', #? 대각선 부분 그래프
+                           hist_kwds={'bins': 35}, #? 그래프에 넣은 인수 키워드
+                           figsize=(10,6))
 #* 시간에 따른 변화를 나타내는 로그 수익률의 경우 주가지수의 변동성이 높은 시기에 변동성 클러스터가 발생한다.
 #* 이 떄, 시계열의 로그 수익률을 그리면서 히스토그램이나 커널 밀도 추정치를 그릴 수 있다.
 
@@ -26,8 +26,8 @@ rets.dropna(inplace=True)
 #! 두 지수의 선형성 확인
 reg = np.polyfit(rets['.SPX'], rets['.VIX'], deg=1) #? OLS 선형회귀 구현
 
-# ax = rets.plot(kind='scatter', x='.SPX', y='.VIX', figsize=(10,6)) #? 로그 수익률 스캐터 플롯
-# ax.plot(rets['.SPX'], np.polyval(reg, rets['.SPX']), 'r', lw=2) #? 선형회귀선
+ax = rets.plot(kind='scatter', x='.SPX', y='.VIX', figsize=(10,6)) #? 로그 수익률 스캐터 플롯
+ax.plot(rets['.SPX'], np.polyval(reg, rets['.SPX']), 'r', lw=2) #? 선형회귀선
 
 # TODO: 상관관계 측정
 rets.corr() #? 전체 Dataframe에 대한 상관관계 행렬
