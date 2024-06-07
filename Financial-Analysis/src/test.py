6f3b4c0f682bb3015acda46796da938ee168528f
junsu0302 <junsu96378224@gmail.com>
2024-06-07 17:28:30 +0900

    #14 - 유럽형 옵션 가치 평가
    유럽형 옵션 가치 평가 클래스 구현
    
    해당 클래스를 이용하는 방법은 test.py로 정리

--- Financial-Analysis/src/test.py
+++ Financial-Analysis/src/test.py
@@ -3,33 +3,55 @@ import pandas as pd
 import datetime as dt
 import matplotlib.pyplot as plt
 
-from Valuation.market_environment import MarketEnvironment
-from Valuation.constant_short_rate import ConstantShortRate
-from Simulation.square_root_diffusion import SquareRootDiffusion
+from Environment.market_environment import MarketEnvironment
+from Utils.constant_short_rate import ConstantShortRate
+from Simulation.geometric_brownian_motion import GeometricBrownianMotion
+from Valuation.mcs_european import MCSEuropean
+from Utils.plot_option_stats import plot_option_stats
 
 #? 시장 환경 설정
 env = MarketEnvironment('env', dt.datetime(2020, 1, 1))
-env.add_constant('initial_value', .25)
-env.add_constant('volatility', 0.05)
-env.add_constant('final_date', dt.datetime(2020, 12, 31))
-env.add_constant('currency', 'EUR')
-env.add_constant('frequency', 'W')
-env.add_constant('paths', 10000)
-env.add_constant('kappa', 4.0)
-env.add_constant('theta', 0.2)
+env.add_constant('initial_value', 36.) #* 초기 자산 가격
+env.add_constant('volatility', 0.2) #* 자산 가격 변동성
+env.add_constant('final_date', dt.datetime(2020, 12, 31)) #* 시뮬레이션 종료 날짜
+env.add_constant('currency', 'EUR') #* 통화
+env.add_constant('frequency', 'M') #* 시뮬레이션 빈도
+env.add_constant('paths', 10000) #* 시뮬레이션 경로 수
 
-#? 할인율 곡선 설정
-csr = ConstantShortRate('r', 0.0)
+#? 할인 곡선 생성 및 추가
+csr = ConstantShortRate('csr', 0.06)
 env.add_curve('discount_curve', csr)
 
-#? GBM 시뮬레이션 객체 생성 및 시간 그리드 생성
-srd = SquareRootDiffusion('srd', env)
+#? 기하 브라운 운동 시뮬레이션 객체 생성
+gbm = GeometricBrownianMotion('gbm', env)
 
-paths = srd.get_instrument_values()[:, :10]
+#? 옵션 시장 환경 설정
+call = MarketEnvironment('call', env.pricing_date)
+call.add_constant('strike', 40.)
+call.add_constant('maturity', dt.datetime(2020, 12, 31))
+call.add_constant('currency', 'EUR')
+
+#? 옵션 페이오프 설정
+payoff_func = 'np.maximum(maturity_value - strike, 0)'
+
+#? 유럽형 옵션 평가 객체 생성
+eur_call = MCSEuropean('eur_call', underlying=gbm, market_env=call, payoff_func=payoff_func)
+
+#? 옵션의 현재 가치, 델타, 베가 계산
+eur_call.present_value()
+eur_call.delta()
+eur_call.vega()
+
+#? 다양한 기초 자산 가격에 대해 옵션 가격, 델타, 베가 계산
+s_list = np.arange(34., 46.1, 2.) #* 기초 자산 가격
+p_list = [] #* 기초 자산 가격에 대한 옵션의 현재 가치
+d_list = [] #* 기초 자산 가격에 대한 옵션의 델타 값
+v_list = [] #* 기초 자산 가격에 대한 옵션의 베가 값
+for s in s_list:
+  eur_call.update(initial_value=s)
+  p_list.append(eur_call.present_value(fixed_seed=True))
+  d_list.append(eur_call.delta())
+  v_list.append(eur_call.vega())
 
 #? 시각화
-plt.figure(figsize=(10, 6))
-plt.plot(srd.time_grid, paths)
-plt.axhline(env.get_constant('theta'), color='r', ls='--', lw=2.0)
-plt.xticks(rotation=30)
-plt.show()
+plot_option_stats(s_list, p_list, d_list, v_list)
