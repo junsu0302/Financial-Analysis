3ced68ff28eb9e661348b7ea7d6e7030528cef97
junsu0302 <junsu96378224@gmail.com>
2024-06-08 00:37:03 +0900

    #14 - 미국식 옵션 가치 평가
    미국식 옵션 가치 평가 클래스 구현
    
    해당 클래스를 이용하는 방법은 test.py로 정리

--- Financial-Analysis/src/test.py
+++ Financial-Analysis/src/test.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from Environment.market_environment import MarketEnvironment
 from Utils.constant_short_rate import ConstantShortRate
 from Simulation.geometric_brownian_motion import GeometricBrownianMotion
-from Valuation.mcs_european import MCSEuropean
+from Valuation.mcs_american import MCSAmerican
 from Utils.plot_option_stats import plot_option_stats
 
 #? 시장 환경 설정
@@ -15,8 +15,8 @@ env.add_constant('initial_value', 36.) #* 초기 자산 가격
 env.add_constant('volatility', 0.2) #* 자산 가격 변동성
 env.add_constant('final_date', dt.datetime(2020, 12, 31)) #* 시뮬레이션 종료 날짜
 env.add_constant('currency', 'EUR') #* 통화
-env.add_constant('frequency', 'M') #* 시뮬레이션 빈도
-env.add_constant('paths', 10000) #* 시뮬레이션 경로 수
+env.add_constant('frequency', 'W') #* 시뮬레이션 빈도
+env.add_constant('paths', 50000) #* 시뮬레이션 경로 수
 
 #? 할인 곡선 생성 및 추가
 csr = ConstantShortRate('csr', 0.06)
@@ -26,32 +26,27 @@ env.add_curve('discount_curve', csr)
 gbm = GeometricBrownianMotion('gbm', env)
 
 #? 옵션 시장 환경 설정
-call = MarketEnvironment('call', env.pricing_date)
-call.add_constant('strike', 40.)
-call.add_constant('maturity', dt.datetime(2020, 12, 31))
-call.add_constant('currency', 'EUR')
+put = MarketEnvironment('put', env.pricing_date)
+put.add_constant('strike', 40.)
+put.add_constant('maturity', dt.datetime(2020, 12, 31))
+put.add_constant('currency', 'EUR')
 
 #? 옵션 페이오프 설정
-payoff_func = 'np.maximum(maturity_value - strike, 0)'
+payoff_func = 'np.maximum(strike - instrument_values, 0)'
 
 #? 유럽형 옵션 평가 객체 생성
-eur_call = MCSEuropean('eur_call', underlying=gbm, market_env=call, payoff_func=payoff_func)
+am_put = MCSAmerican('am_put', underlying=gbm, market_env=put, payoff_func=payoff_func)
 
-#? 옵션의 현재 가치, 델타, 베가 계산
-eur_call.present_value()
-eur_call.delta()
-eur_call.vega()
+am_put.present_value(fixed_seed=True, bf=5)
 
-#? 다양한 기초 자산 가격에 대해 옵션 가격, 델타, 베가 계산
-s_list = np.arange(34., 46.1, 2.) #* 기초 자산 가격
-p_list = [] #* 기초 자산 가격에 대한 옵션의 현재 가치
-d_list = [] #* 기초 자산 가격에 대한 옵션의 델타 값
-v_list = [] #* 기초 자산 가격에 대한 옵션의 베가 값
-for s in s_list:
-  eur_call.update(initial_value=s)
-  p_list.append(eur_call.present_value(fixed_seed=True))
-  d_list.append(eur_call.delta())
-  v_list.append(eur_call.vega())
+ls_table = []
+for initial_value in (36., 38., 40., 42., 44.):
+  for volatility in (0.2, 0.4):
+    for maturity in (dt.datetime(2020, 12, 31), dt.datetime(2021, 12, 31)):
+      am_put.update(initial_value=initial_value, volatility=volatility, maturity=maturity)
+      ls_table.append([initial_value, volatility, maturity, am_put.present_value(bf=5)])
 
-#? 시각화
-plot_option_stats(s_list, p_list, d_list, v_list)
+print('S0 | Vola | T | Value')
+print(22 * '-')
+for r in ls_table:
+  print('%d | %3.1f | %d | %5.3f' % (r[0], r[1], r[2].year - 2019, r[3]))
